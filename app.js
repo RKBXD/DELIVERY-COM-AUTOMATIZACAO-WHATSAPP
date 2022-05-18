72227bb3a0cc71af66b2d5d0d593737a69cde1e6
Erik Pereira Rodrigues <rodrigues.erik.jr@gmail.com>
2022-05-17 22:43:50 -0300

    APP - ATT

---
+++ app.js
@@ -0,0 +1,8 @@
+var doisad = document.getElementById('doisad')
+var complementos = document.getElementById('complementos')
+var complementosdois = document.getElementById('complementosdois')
+
+function clicar () {complementos.style.display='flex'}
+function clicard () {complementosdois.style.display='flex'}
+function clicarFechar () {complementos.style.display='none'}
+function clicarFechard () {complementosdois.style.display='none'}
